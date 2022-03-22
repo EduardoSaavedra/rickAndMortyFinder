d855a5bcb6fb8ebd2fd42c74ec704e34ea7f80a4
Eduardo Saavedra <eduardo.saavedra@resuelve.mx>
2022-03-22 14:37:50 -0600

    delete env

--- src/test/index.js
+++ src/test/index.js
@@ -8,8 +8,7 @@ const ServiceTerms = () => {
       debidamente constituida en México, cuyo nombre comercial es “ENCONTA”. Por su parte el
       “Cliente” en este acto declara ser una persona física, mayor de edad y contar con
       capacidad para contratar y contraer toda clase de obligaciones y que entiende plenamente
-      el contenido de los presentes Términos y Condiciones. Por lo que, tanto el “Cliente” como
-      
+      el contenido de los presentes Términos y Condiciones. Por lo que, tanto el “Cliente” como      
     </p>
   )
 }
